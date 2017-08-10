20d37f4facd63f6d1f026f954c6e1432f5c6c738
Andres Moschini <andresmoschini@gmail.com>
2017-08-10 16:53:13 -0300

    Add support for Tasks in all supported frameworks

---
+++ Src/MakingSense.SafeBrowsing/Internal/TaskUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingSense.SafeBrowsing.Internal
+{
+    /// <summary>
+    /// Utilities to simplify Task usage in different frameworks
+    /// </summary>
+    public static class TaskUtilities
+    {
+        /// <summary>
+        /// Creates a System.Threading.Tasks.Task`1 that's completed successfully with the
+        /// specified result.
+        /// </summary>
+        /// <typeparam name="T">The type of the result returned by the task.</typeparam>
+        /// <param name="result">The result to store into the completed task.</param>
+        /// <returns>The successfully completed task.</returns>
+        public static Task<T> FromResult<T>(T result)
+#if (HAVE_ASYNC)
+            => Task.FromResult(result);
+#else
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetResult(result);
+            return tcs.Task;
+        }
+#endif
+
+        private static Task _completedTask = null;
+        public static Task CompletedTask => _completedTask ?? (_completedTask = FromResult(true));
+    }
+}

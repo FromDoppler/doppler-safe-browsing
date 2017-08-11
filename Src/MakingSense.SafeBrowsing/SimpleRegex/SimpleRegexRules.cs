8b504dfd16cb4e47b78623200b94706356718be8
Andres Moschini <andresmoschini@gmail.com>
2017-08-11 11:11:18 -0300

    Introduce SimpleRegexRules

---
+++ Src/MakingSense.SafeBrowsing/SimpleRegex/SimpleRegexRules.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MakingSense.SafeBrowsing.SimpleRegex
+{
+    /// <summary>
+    /// Set of rules necessary to evaluate links by SimpleRegexUrlChecker
+    /// </summary>
+    public class SimpleRegexRules
+    {
+        private static readonly ReadOnlyCollection<Regex> EmptyCollection = new ReadOnlyCollection<Regex>(new List<Regex>());
+
+        /// <summary>
+        /// Collection of regular expressions that represent dangerous URLs
+        /// </summary>
+        public ReadOnlyCollection<Regex> Blacklist { get; private set; }
+
+        /// <summary>
+        /// Create SimpleRegexRules without any blacklist entry
+        /// </summary>
+        public SimpleRegexRules()
+        {
+            Blacklist = EmptyCollection;
+        }
+
+        /// <summary>
+        /// Create SimpleRegexRules with specified blacklist entry
+        /// </summary>
+        /// <param name="initialBlacklist"></param>
+        public SimpleRegexRules(IEnumerable<Regex> initialBlacklist)
+        {
+            Update(initialBlacklist);
+        }
+
+        /// <summary>
+        /// Update current blacklist with a new one
+        /// </summary>
+        /// <param name="initialBlacklist"></param>
+        public void Update(IEnumerable<Regex> initialBlacklist)
+        {
+            Blacklist = new ReadOnlyCollection<Regex>(initialBlacklist.ToList());
+        }
+    }
+}

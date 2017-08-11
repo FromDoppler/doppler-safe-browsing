14e95170d53a83ddd6db8300fd9440cace6ad35a
Andres Moschini <andresmoschini@gmail.com>
2017-08-11 16:54:40 -0300

    Add HttpClient abstraction

---
+++ Src/MakingSense.SafeBrowsing.Tests/HttpClientDouble.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingSense.SafeBrowsing.Tests
+{
+    public class HttpClientDouble : IHttpClient
+    {
+        public string Response_GetString { get; set; }
+
+        public Task<string> GetStringAsync(string url)
+        {
+            return Internal.TaskUtilities.FromResult(Response_GetString);
+        }
+    }
+}
---
+++ Src/MakingSense.SafeBrowsing/Internal/HttpClient.cs
@@ -0,0 +1,26 @@
+﻿#if (!NETSTANDARD1_0)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingSense.SafeBrowsing.Internal
+{
+    /// <summary>
+    /// Wrapper around System.Net.Http.HttpClient to allow easily implement a compatible version for netstandard1.0
+    /// </summary>
+    public class HttpClient : IHttpClient
+    {
+
+        private static System.Net.Http.HttpClient _httpClient = new System.Net.Http.HttpClient();
+
+        /// <inheritdoc />
+        public Task<string> GetStringAsync(string url)
+        {
+            return _httpClient.GetStringAsync(url);
+        }
+    }
+}
+#endif

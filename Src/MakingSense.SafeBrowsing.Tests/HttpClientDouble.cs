bab811a34a3c6b66bb75c163e7ab1869a88cb6b4
Andres Moschini <andresmoschini@gmail.com>
2017-08-14 10:35:32 -0300

    Improve HttpClientDouble

--- Src/MakingSense.SafeBrowsing.Tests/HttpClientDouble.cs
+++ Src/MakingSense.SafeBrowsing.Tests/HttpClientDouble.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#pragma warning disable 1998 // Because I want to generate tasks from synchronous code
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,55 @@ namespace MakingSense.SafeBrowsing.Tests
 {
     public class HttpClientDouble : IHttpClient
     {
-        public string Response_GetString { get; set; }
+        private Func<string, string, SimplifiedHttpResponse> _overrideGetString = (a, b) => new SimplifiedHttpResponse() { Body = string.Empty };
 
-        public Task<SimplifiedHttpResponse> GetStringAsync(string url, string ifNoneMatch = null)
-        {
-            return Internal.TaskUtilities.FromResult(new SimplifiedHttpResponse()
+        /// <summary>
+        /// Set an exception fpr GetStringAsync
+        /// </summary>
+        /// <param name="exception"></param>
+        public void Setup_GetString(Exception exception) =>
+            _overrideGetString = (url, ifNoneMatch) => throw exception;
+
+        /// <summary>
+        /// Set a result for GetStringAsync
+        /// </summary>
+        /// <param name="func"></param>
+        public void Setup_GetString(string responseBody) =>
+            _overrideGetString = (url, ifNoneMatch) => new SimplifiedHttpResponse()
             {
-                Body = Response_GetString
-                // TODO: support setting of NotModified and Etag values
-            });
-        }
+                Body = responseBody
+            };
+
+        /// <summary>
+        /// Set a result for GetStringAsync
+        /// </summary>
+        /// <param name="result"></param>
+        public void Setup_GetString(SimplifiedHttpResponse result) =>
+            _overrideGetString = (url, ifNoneMatch) => result;
+
+        /// <summary>
+        /// Override GetStringAsync behavior
+        /// </summary>
+        /// <param name="func"></param>
+        public void Setup_GetString(Func<SimplifiedHttpResponse> func) =>
+            _overrideGetString = (url, ifNoneMatch) => func();
+
+        /// <summary>
+        /// Override GetStringAsync behavior
+        /// </summary>
+        /// <param name="func"></param>
+        public void Setup_GetString(Func<string, SimplifiedHttpResponse> func) =>
+            _overrideGetString = (url, ifNoneMatch) => func(url);
+
+        /// <summary>
+        /// Override GetStringAsync behavior
+        /// </summary>
+        /// <param name="func"></param>
+        public void Setup_GetString(Func<string, string, SimplifiedHttpResponse> func) =>
+            _overrideGetString = func;
+
+        public async Task<SimplifiedHttpResponse> GetStringAsync(string url, string ifNoneMatch = null) =>
+            _overrideGetString(url, ifNoneMatch);
     }
 }
+#pragma warning restore 1998
